3c38cb40c26b957a2e4b5364e36cb0bf4b72c207
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-18 14:49:07 +0200

    Update build.gradle.kts

--- service-seed/build.gradle.kts
+++ service-seed/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("com.vaadin") version "0.14.3.7"
 	kotlin("jvm") version "1.4.32"
 	kotlin("plugin.spring") version "1.4.32"
 	kotlin("plugin.jpa") version "1.4.32"
